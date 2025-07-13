3bfab8b7243d9d3f95ccefe0fbfc255adaff492e
ihsansengun <me@ihsansengun.com>
2025-07-13 03:13:57 +0300

    Integrate Clerk and Supabase

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,5 +1,37 @@
-function CompanionsLibrary() {
-  return <div> Companions Library Page</div>;
-}
+import { getAllCompanions } from "@/lib/actions/Companion.actions";
+import CompanionCard from "@/components/CompanionCard";
+import { getSubjectColor } from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SubjectFilter from "@/components/SubjectFilter";
+
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const tutors = await getAllCompanions({ subject, topic });
+  console.log(tutors);
+
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Tutor Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SubjectFilter />
+        </div>
+      </section>
+      <section className="companions-grid">
+        {tutors?.map((tutor) => (
+          <CompanionCard
+            key={tutor.id}
+            {...tutor}
+            color={getSubjectColor(tutor.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
+};
 
 export default CompanionsLibrary;
